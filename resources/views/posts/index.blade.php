5efa9e6e9cf4eb4e9daeebbdf346ba08dc126c68
yuta0531 <torimoto.jobhunting@gmail.com>
2023-08-01 15:09:38 +0900

    08-1

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        
+        <title>Blog</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    </head>
+    <body class="antialiased">
+       <h1>Blog name</h1>
+       <div class='posts'>
+           <div class='post'>
+               <h2 class='title'>Title1</h2>
+               <p class 'body'>This is a sample body</p>
+           </div>
+       </div>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PostController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//Route::get('/', function () { return view('welcome'); });
+/*
+Route::get('/' ,function(){
+    return view('posts/index');
 });
+*/
+
+Route::get('/posts',[PostController::class, 'index']);
