86c3db6c983f43cc8c9390cae8839dfc0b657e84
yuta0531 <torimoto.jobhunting@gmail.com>
2023-08-11 16:06:45 +0900

    8-6

---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        
+        <title>Blog</title>
+
+        <!-- Fonts -->
+       <!--<link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet"> -->
+
+    </head>
+    <body>
+       <h1>Blog Name</h1>
+       <form action="/posts/{{ $post->id }}" method="POST">
+                @csrf
+                @method('PUT')
+            <div class="content_title">
+               <h2>Title</h2>
+               <input type="text" name="post[title]" value="{{$post->title}}">
+           </div>
+           <div class="body">
+               <h2>Body</h2>
+               <input type="text" name="post[title]" value="{{$post->body}}">
+<!--               <textarea name="post[body]" value="{$post->body}}"></textarea>  -->
+           </div>
+           <input type="submit" value="update">
+       </form>
+       <div class="footer">
+           <a href="/posts/{{$post->id}}">戻る</a>
+       </div>
+    </body>
+</html>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -17,9 +17,24 @@
                <div class='post'>
                    <a href="/posts/{{$post->id}}"><h2 class='title'>{{ $post->title}}</h2></a>
                    <p class 'body'>{{$post ->body}}</p>
+                   <form action="/posts/{{$post->id}}" id="form_{{$post->id}}" method="post">
+                       @csrf
+                       @method('DELETE')
+                       <button type="buttun" onclick="deletePost({{$post->id}})">delete</button>
+                   </form>
                </div>
            @endforeach
        </div>
        <div class='paginate'>{{$posts->links()}}</div>
+       
+       <script>
+           function deletePost(id){
+               'use scrict'
+               
+               if(confirm('消去すると復元できません。\n本当に削除しますか？')) {
+                   document.getElementById(`form_${id}`).submit();
+               }
+           }
+       </script>
     </body>
 </html>
