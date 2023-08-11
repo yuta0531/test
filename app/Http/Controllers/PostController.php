96ce6f8c3baccfa251840044779adaf5090c02ad
yuta0531 <torimoto.jobhunting@gmail.com>
2023-08-11 13:20:34 +0900

    re8-4

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -30,7 +30,7 @@ class PostController extends Controller
     
     public function create()
     {
-        return view('posts/create');
+        return view('posts.create');
     }
     
     public function store(PostRequest $request, Post $post)
@@ -39,4 +39,17 @@ class PostController extends Controller
         $post->fill($input)->save();
         return redirect('/posts/' . $post->id);
     }
+    
+    public function edit(Post $post)
+    {
+        return view('posts.edit')->with(['post' => $post]);
+    }
+    
+    public function update(PostRequest $request , Post $post)
+    {
+        $input_post=$request['post'];
+        $post->fill($input_post)->save();
+        return redirect('/posts/' . $post->id);
+    }
+    
 }
